9f4e890a5e01e6facdb55fc6778858eace742e0d
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-01-10 00:12:41 -0800

    i

---
+++ homework2/public/javascripts/control.js
@@ -0,0 +1,137 @@
+var Orders = (function () {
+    'use strict';
+    var data = null;
+    var order = null;
+
+    // constructor
+    function Orders() {
+        $('#ordersButton').click(generateOrders);
+        $('#generateOrder').click(generateOrderHandler);
+        $('#saveOrder').click(saveOrderToDb);
+    }
+
+    Orders.prototype.importData = function () {
+        $.getJSON('data.json', function (result) {
+            data = result;
+        });
+    };
+
+    function generateOrders(){
+
+        var numberOfOrders =  $('#numberOfOrders').val();
+        if(numberOfOrders&&!isNaN(numberOfOrders)){
+            for (var i = 0; i < numberOfOrders; i++) {
+                generateOrder();
+                if(order){
+                    saveOrderToDb();
+                }
+            }
+        }
+        else{
+            alert('Please enter a valid number of orders to be generated');
+        }
+    }
+
+    function generateOrderHandler() {
+        generateOrder();
+        displayOrder();
+    }
+
+    function generateOrder() {
+
+        var storeNumber = null;
+        var salesPersonId = null;
+        var itemNumber = null;
+        var pricePaid = null;
+        var timePunch = null;
+
+        //var order = null;
+
+        if (data.hasOwnProperty('storeInfo') && data.hasOwnProperty('itemNumbers') &&
+            data.hasOwnProperty('itemMaxPrice') && data.hasOwnProperty('itemMinPrice')) {
+
+            var storeInfo = generateNumber(data['storeInfo']);
+            if (storeInfo.hasOwnProperty('zipCode')) {
+                storeNumber = storeInfo['zipCode'];
+                salesPersonId = generateNumber(storeInfo['salesPersonsIds']);
+            }
+            itemNumber = generateNumber(data['itemNumbers']);
+            pricePaid = Math.floor(Math.random() * ((data['itemMaxPrice'] - data['itemMinPrice']) + 1)) + data['itemMinPrice'];
+            timePunch = generateRandomTime();
+
+            order = {
+                storeNumber: storeNumber,
+                salesPersonId: salesPersonId,
+                itemNumber: itemNumber,
+                pricePaid: pricePaid,
+                timePunch: timePunch
+            };
+        }
+        return order;
+    }
+
+    function saveOrderToDb() {
+        $.post('/insertOrder', order, function (result) {
+            console.log(order);
+            showOrderStatus(result);
+        }, 'json').done(function() {
+            console.log('Done saving to database');
+        }).fail(function(jqxhr, textStatus, error) {
+            console.log('error: ' + jqxhr.status + ' ' + textStatus + ' ' + error);
+        });
+    }
+
+    function displayOrder() {
+
+        if (order) {
+            for (var property in order) {
+                if (order.hasOwnProperty(property)) {
+                    var controlId = '#' + property;
+                    $(controlId).val(order[property]);
+                }
+            }
+        }
+    }
+
+    function generateNumber(array) {
+        var random = Math.floor(Math.random() * array.length);
+        return array[random];
+    }
+
+    function generateRandomTime() {
+        var minSeconds = 2;
+        var maxSeconds = 120;
+        var randomSeconds = Math.floor(Math.random() * ((maxSeconds - minSeconds) + 1)) + minSeconds;
+
+        var currentdate = new Date();
+        currentdate.setSeconds(currentdate.getSeconds() + randomSeconds);
+
+        var timePunch = currentdate.getDate() + "/"
+            + (currentdate.getMonth() + 1) + "/"
+            + currentdate.getFullYear() + " @ "
+            + currentdate.getHours() + ":"
+            + currentdate.getMinutes() + ":"
+            + currentdate.getSeconds();
+
+        return timePunch;
+    }
+
+    function showOrderStatus(result) {
+
+        if (result.result == 'success') {
+            $('#ordersPlacementStatus').html('Order(s) saved successfully');
+        }
+        else {
+            $('#ordersPlacementStatus').html('Order(s) failed to save to the database');
+        }
+    };
+
+    return Orders;
+}());
+
+$(document).ready(function () {
+    'use strict';
+    var myOrders = new Orders();
+    myOrders.importData();
+});
+
---
+++ homework2/routes/index.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) { 'use strict';
+  res.render('index', { title: 'Thanh & Sean OrdersApp' });
+});
+
+router.get('/orders', function(req, res) {
+    var db = req.db;
+    var collection = db.get('Usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('orders', {
+            "orders" : docs
+        });
+    });
+});
+
+var count = 0;
+
+router.post('/addorder', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+
+    // Set our collection
+    var collection = db.get('Usercollection');
+
+        // Submit to the DB
+        collection.insert({
+            "storeNumber": req.body.storeNumber,
+            "salesPersonID": req.body.salesPersonID,
+            "itemNumber": req.body.itemNumber,
+            "timePurch": req.body.timePurch,
+            "pricePaid": req.body.pricePaid
+        }, function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send({
+                    result: 'There was a problem inserting a record into the database'
+                });}
+            else {
+                // And forward to success page
+                /*res.send({
+                    'Result': 'Success'
+                });*/
+                res.send({
+                    result: 'success'
+                });
+
+            }
+        });// end loop
+   // res.redirect('/');
+});
+
+
+
+module.exports = router;
